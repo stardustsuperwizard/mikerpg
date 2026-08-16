4e12cef3337453ad510d83d153fe22a3f40b3ac3
stardustsuperwizard <47020213+stardustsuperwizard@users.noreply.github.com>
2026-08-15 20:11:31 -0400

    yup

--- core/actions/action_result.gd
+++
@@ -1,9 +0,0 @@
-class_name ActionResult
-extends RefCounted
-
-var success: bool
-var reason: StringName
-
-func _init(p_success: bool = true, p_reason: StringName = &"") -> void:
-	success = p_success
-	reason = p_reason
--- core/world/spawn_point.gd
+++
@@ -1,7 +0,0 @@
-class_name SpawnPoint
-extends Resource
-
-@export var actor_scene: PackedScene
-@export var character_sheet: CharacterSheet
-@export var color: Color = Color.WHITE
-@export var transform: Transform3D = Transform3D.IDENTITY
